bdc93f7daa3f3ab4b131b7b64e697f8b581cad82
Joe Rosenthal <josephrosenthal@gmail.com>
2021-03-27 19:26:08 -0700

    Updated PlayerController
    Also added a test scene for movement and a few tilemap grids

--- Arena/Assets/Scripts/Simple2dPlatformController.cs
+++ Arena/Assets/Scripts/Simple2dPlatformController.cs
@@ -13,33 +13,45 @@ using System.Collections;
  *  Edge Colliders are recommended for Platforms that you intend to drop down from.
 */
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Simple2dPlatformController : MonoBehaviour
 {
     private bool wasJumpPressed;
     private bool platformDropDown = false;
     private PlayerControls playerControls;
+    private PlayerAnimationHandler playerAnimationHandler;
+    private bool jumpHeld;
 
     #region Public Variables, hidden from inspector
     /// <summary>
     /// Other scripts might want to access these at some point
     /// </summary>
-    [HideInInspector]
+    //[HideInInspector]
     public bool isGrounded = false;
-    [HideInInspector]
+    //[HideInInspector]
     public Vector3 Velocity;
     #endregion
 
     #region Public Inspector Variables
     public bool DigitalKeyboardControls = false;
-    public float Gravity = -9.81f;
-    public float JumpStrength = 6f;
+    public float VelocityClamp = 0.05f;
+    public float HorizontalDeadZone = 0.1f;
+    public float VerticalDeadZone = 0.3f;
+    public float Gravity = -20f;
+    public float JumpStrength = 10f;
+    public float RunSpeed = 5f;
+    public bool UseAcceleration = false;
     public float Acceleration = 10f;
+    public float Deceleration = 10f;
     public float SkidAcceleration = 20f;
+    public bool UseSkidAcceleration = false;
     public float ColliderSize = 0.15f;
     public float MaximumHorizontalSpeed = 10f;
-    public float MaximumVerticalSpeed = 10f;
+    public float MaximumVerticalSpeed = 8f;
     public float platformDropDownCoolDown = 0.2f;
+    public float FallMultiplier = 2.5f;
+    public float LowJumpMultipler = 2f;
 
     /// <summary>
     /// At minimum you want 2 layers. One for your player and one for your geometry.
@@ -55,12 +67,13 @@ public class Simple2dPlatformController : MonoBehaviour
     /// </summary>
     void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(this.transform.position, Vector3.one * ColliderSize * 2);
+        Gizmos.DrawWireCube(GetComponent<SpriteRenderer>().bounds.center, new Vector3(GetComponent<SpriteRenderer>().bounds.extents.x * 2, GetComponent<SpriteRenderer>().bounds.extents.y * 2, 0f));
     }
 
     private void Awake()
     {
         playerControls = new PlayerControls();
+        playerAnimationHandler = GetComponent<PlayerAnimationHandler>();
     }
 
     private void OnEnable()
@@ -80,8 +93,27 @@ public class Simple2dPlatformController : MonoBehaviour
         //var verticalMovement = DigitalKeyboardControls ? Input.GetAxisRaw("Vertical") : Input.GetAxis("Vertical");
 
         //Player Input
-        var horizontalMovement = playerControls.InGame.Horizontal.ReadValue<float>();
-        var verticalMovement = playerControls.InGame.Vertical.ReadValue<float>();
+
+        float horizontalMovement = 0f;
+        float verticalMovement = 0f;
+
+        if(Mathf.Abs(playerControls.InGame.Horizontal.ReadValue<float>()) > HorizontalDeadZone)
+        {
+            horizontalMovement = playerControls.InGame.Horizontal.ReadValue<float>();
+        }
+        else
+        {
+            horizontalMovement = 0f;
+        }
+
+        if (Mathf.Abs(playerControls.InGame.Vertical.ReadValue<float>()) > VerticalDeadZone)
+        {
+            verticalMovement = playerControls.InGame.Vertical.ReadValue<float>();
+        }
+        else
+        {
+            verticalMovement = 0f;
+        }
 
         var canJump = GetCanJump();
 
@@ -103,18 +135,43 @@ public class Simple2dPlatformController : MonoBehaviour
         {
             if (Velocity.x == 0 || Mathf.Sign(horizontalMovement) == Mathf.Sign(Velocity.x))
             {
-                Velocity.x += horizontalMovement * Acceleration * Time.deltaTime;
+
+                //Added a Custom Toggle here for Velocity
+                if (UseAcceleration == true)
+                {
+                    Velocity.x += horizontalMovement * Acceleration * RunSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    Velocity.x = horizontalMovement * RunSpeed;
+                }
+
             }
             else //If we're moving in the opposite direction, skid.
             {
-                Velocity.x += horizontalMovement * SkidAcceleration * Time.deltaTime;
+
+                //Added a Custom Toggle here for Skid Velocity.
+                if (UseSkidAcceleration == true)
+                {
+                    Velocity.x += horizontalMovement * SkidAcceleration * RunSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    Velocity.x = horizontalMovement * RunSpeed;
+                }
+
             }
         }
         else if (Velocity.x != 0)
         {
-            Velocity.x -= Mathf.Sign(Velocity.x) * Acceleration * Time.deltaTime;
-            Velocity.x = Velocity.x < 0.001f ? (Velocity.x > -0.001f ? 0 : Velocity.x) : Velocity.x; //set to 0 if it's close enough
+            //Velocity.x -= Mathf.Sign(Velocity.x) * Deceleration * Time.deltaTime;
+            Velocity.x -= Velocity.x * Deceleration * Time.deltaTime;
+            Velocity.x = Velocity.x < VelocityClamp ? (Velocity.x > -VelocityClamp ? 0 : Velocity.x) : Velocity.x; //set to 0 if it's close enough
         }
+
+        //Send horizontal movement to the animation handler.
+        //Eventually, we want to expand this functionality to send many inputs to animation handler.
+        playerAnimationHandler.UpdateAnimatorValues(horizontalMovement);
 
         //Clamp to maximum
         Velocity.x = Mathf.Clamp(Velocity.x, -MaximumHorizontalSpeed, MaximumHorizontalSpeed);
@@ -133,17 +190,7 @@ public class Simple2dPlatformController : MonoBehaviour
         //Old Input System
         //var jumpButtonDown = Input.GetButton("Jump");
 
-        var jumpValue = playerControls.InGame.Jump.ReadValue<float>();
-        bool jumpButtonDown;
-
-        if (jumpValue > 0)
-        {
-            jumpButtonDown = true;
-        }
-        else
-        {
-            jumpButtonDown = false;
-        }
+        bool jumpButtonDown = playerControls.InGame.Jump.triggered;
 
         //If we have previously pressed the jump button and the jump button has been released
         if (wasJumpPressed && !jumpButtonDown)
@@ -155,6 +202,7 @@ public class Simple2dPlatformController : MonoBehaviour
             wasJumpPressed = true; //Disable jumping
             return true; //tell the parent that we've jumped
         }
+
         return false; //Can't Jump, Won't Jump
 
     }
@@ -181,8 +229,21 @@ public class Simple2dPlatformController : MonoBehaviour
         //Add gravity if we're off the ground
         if (!isGrounded)
         {
-            Velocity.y += Gravity * Time.fixedDeltaTime;
+            if(Velocity.y < 0)
+            {
+                Velocity += Vector3.up * Gravity * (FallMultiplier) * Time.deltaTime;
+            } else if (Velocity.y > 0 && !InputSystem.GetDevice<Gamepad>().buttonSouth.isPressed) //Right now, we're checking the GamePad directly. This is not flexible. Let's change this at some point.
+            {
+                Debug.Log("We are in the air AND jump isn't being pressed");
+                Velocity += Vector3.up * Gravity * (LowJumpMultipler) * Time.deltaTime;
+            }
+            else
+            {
+                Velocity.y += Gravity * Time.fixedDeltaTime;
+            }
         }
+
+        Debug.Log(InputSystem.GetDevice<Gamepad>().buttonSouth.isPressed);
 
         //Move the player
         this.transform.position += Velocity * Time.fixedDeltaTime;
